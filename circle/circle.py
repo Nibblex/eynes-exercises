03aac65366b80c2d632712f1567311213f956659
Sergio Rodriguez <sergio_mu12@hotmail.com>
2023-10-30 21:44:02 -0300

    Ejercicio - circle

--- circle/circle.py
+++ circle/circle.py
@@ -1,4 +1,30 @@
 import math
 
-class Circle():
-    pass
+
+class Circle:
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError("El radio no puede ser negativo")
+        self._radius = radius
+
+    def get_radius(self):
+        return self._radius
+
+    def set_radius(self, radius):
+        if radius <= 0:
+            raise ValueError("El radio no puede ser negativo")
+        self._radius = radius
+
+    def get_area(self):
+        return math.pi * self._radius**2
+
+    def get_perimeter(self):
+        return 2 * math.pi * self._radius
+
+    def __mul__(self, radius):
+        if radius <= 0:
+            raise ValueError("El multiplicador no puede ser negativo")
+        return Circle(self._radius * radius)
+
+    def __str__(self):
+        return f"Circulo de radio {self._radius}"
